947d97e022fd80cf0b97bd597de44687a273ab17
katsu-victory <137839280+katsu-victory@users.noreply.github.com>
2024-11-15 15:55:35 +0900

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getUserCount(userId) {
     try {
-        const response = await fetch(`https://script.google.com/macros/s/AKfycbwq7z-Jsfy0rfT-roWRjcJ5csYRVO6t7iOoFOwnvfoNPCfIyPGUCnlsQPx2T9qRx1oV/exec?userId=${userId}`);
+        const response = await fetch(`https://script.google.com/macros/s/AKfycbx9H_hc4CthmcTD33THwf3DDjMlq9tQ469nFIYmeJjtgV5EqibrX54s3oRKpOptdP38/exec?userId=${userId}`);
         if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
         const data = await response.json();
         return data.count || 1;
@@ -116,7 +116,7 @@ async function suggestAndSubmit(event) {
     };
 
     try {
-        await fetch("https://script.google.com/macros/s/AKfycbwq7z-Jsfy0rfT-roWRjcJ5csYRVO6t7iOoFOwnvfoNPCfIyPGUCnlsQPx2T9qRx1oV/exec", {
+        await fetch("https://script.google.com/macros/s/AKfycbx9H_hc4CthmcTD33THwf3DDjMlq9tQ469nFIYmeJjtgV5EqibrX54s3oRKpOptdP38/exec", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             mode: "no-cors",
@@ -142,7 +142,7 @@ function submitFeedback() {
         userId: userId
     };
 
-    fetch("https://script.google.com/macros/s/AKfycbwq7z-Jsfy0rfT-roWRjcJ5csYRVO6t7iOoFOwnvfoNPCfIyPGUCnlsQPx2T9qRx1oV/exec", {
+    fetch("https://script.google.com/macros/s/AKfycbx9H_hc4CthmcTD33THwf3DDjMlq9tQ469nFIYmeJjtgV5EqibrX54s3oRKpOptdP38/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         mode: "no-cors",
@@ -166,7 +166,7 @@ function submitFeedback() {
 }
 async function fetchUserData(userId) {
     try {
-        const response = await fetch("https://script.google.com/macros/s/AKfycbwq7z-Jsfy0rfT-roWRjcJ5csYRVO6t7iOoFOwnvfoNPCfIyPGUCnlsQPx2T9qRx1oV/exec", {
+        const response = await fetch("https://script.google.com/macros/s/AKfycbx9H_hc4CthmcTD33THwf3DDjMlq9tQ469nFIYmeJjtgV5EqibrX54s3oRKpOptdP38/exec", {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
